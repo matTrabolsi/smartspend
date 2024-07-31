d17ee0dc86f0c05b393f7b1b234c4013f484bbcd
mahmoud <mahmoudtarbolsi@gmai.com>
2024-07-31 15:04:58 +0300

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -35,6 +35,11 @@ public class HomeController : Controller
     public IActionResult Expenses()
     {
         var allExpenses = _context.Expenses.ToList();
+        
+        var totalExpenses = allExpenses.Sum(x => x.Value);
+
+        ViewBag.Expenses = totalExpenses;
+
         return View(allExpenses);
     }
 
--- obj/Debug/net8.0/spendsmart.AssemblyInfo.cs
+++ obj/Debug/net8.0/spendsmart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spendsmart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4057a1ddb32d3d36c12c7d93d59ee89349e14cbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb61389fef35c63d21ec8a9edb8d896fce87a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("spendsmart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spendsmart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
